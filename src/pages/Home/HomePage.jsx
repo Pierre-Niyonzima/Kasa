b3aa74bf3b28cd0602004ce61590a38d11faeb91
Pierre-Niyonzima <pierre.niyonzima@gmail.com>
2022-12-29 15:28:08 +0100

    création de la bannière sous le header

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,20 @@
 
 import { Fragment } from "react";
 import Header from "../../components/Header/Header";
+import Banner from "../../img/banner.png"
+import "./homepage.css";
 
 function HomePage() {
 
      return (
     <Fragment>
         <Header homeView={true} />
+            <div className="banner--container">
+                <img src={Banner} alt="Banner" className="banner--img" />
+                <div className="banner--title">
+                    Chez vous, partout et ailleurs
+                </div>
+            </div>
     </Fragment>    
     )
 }
