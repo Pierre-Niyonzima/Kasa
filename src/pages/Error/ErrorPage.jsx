f40d8438c01d0bb58cd4186ffc17c17edaef92c6
Pierre-Niyonzima <pierre.niyonzima@gmail.com>
2022-12-29 19:41:42 +0100

    responsive pqge Qbout

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,9 +1,22 @@
+import React, { Fragment } from "react"
+import { Link } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import "./errorpage.css"
+
 function ErrorPage() {
 
     return (
-        <div>Error
-        </div>
+        <Fragment>
+            <Header />
+            <h3 className="error error--title">404</h3>
+            <h2 className="error error--subtitle">Oups! La page que vous demandez n'existe pas.</h2>
+            <Link to="/">
+                <p className="error error--return">Retourner sur la page d’accueil</p>
+            </Link>
+            <Footer />
+        </Fragment>
     )
 }
 
-export default ErrorPage
+export default ErrorPage;
