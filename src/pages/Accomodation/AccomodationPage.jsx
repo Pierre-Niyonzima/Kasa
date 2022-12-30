b30fd382dab53f042d0e8fdcfa45887c793e3fa2
Pierre-Niyonzima <pierre.niyonzima@gmail.com>
2022-12-30 11:55:11 +0100

    ajout header, footer + dropdown dans les pages logements

--- src/pages/Accomodation/AccomodationPage.jsx
+++ src/pages/Accomodation/AccomodationPage.jsx
@@ -1,9 +1,50 @@
-function AccomodationPage() {
+import React, { Fragment, useState, useEffect } from "react"
+import { useParams, useNavigate} from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import "./accomodationpage.css";
 
-    return (
-        <div>Accomodation
-        </div>
-    )
+
+function AccomodationPage() {
+    const id = useParams();
+    const navigate = useNavigate();
+    let [currentAccomodation, setCurrentAccomodation] = useState(null);
+
+    useEffect(() => {
+    fetch("/logements.json")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.find((accomodation) => accomodation.id === id.id)) {
+          const currentAccodomation = data.find(
+            (accomodation) => accomodation.id === id.id
+          );
+          setCurrentAccomodation(currentAccodomation);
+        } else {
+          navigate("/error");
+        }
+      });
+  }, [navigate, id.id]);
+
+
+  return (
+    <Fragment>
+      <Header />
+        <Fragment>
+         <div className="accomodation--flex">
+            <Dropdown
+              paragraph={currentAccomodation.description}
+              title="Description"
+            />
+            <Dropdown
+              equipments={currentAccomodation.equipments}
+              title="Équipements"
+            />
+          </div>
+      </Fragment>
+      <Footer />
+    </Fragment>
+  );
 }
 
-export default AccomodationPage
+export default AccomodationPage;
