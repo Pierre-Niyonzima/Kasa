a0d5d6982bf2741fc50e9c48346a41822f3cb807
Pierre-Niyonzima <pierre.niyonzima@gmail.com>
2022-12-30 14:04:56 +0100

    gestion des informations de la page logement

--- src/pages/Accomodation/AccomodationPage.jsx
+++ src/pages/Accomodation/AccomodationPage.jsx
@@ -1,19 +1,18 @@
-import React, { Fragment, useState, useEffect } from "react"
-import { useParams, useNavigate} from "react-router-dom"
-import Footer from "../../components/Footer/Footer"
-import Header from "../../components/Header/Header"
-import Dropdown from "../../components/Dropdown/Dropdown"
-import Rating from "../../components/Rating/Rating"
-import Tag from "../../components/Tag/Tag"
-import "./accomodationpage.css"
-
+import React, { Fragment, useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import Rating from "../../components/Rating/Rating";
+import Tag from "../../components/Tag/Tag";
+import "./accomodationpage.css";
 
 function AccomodationPage() {
-    const id = useParams();
-    const navigate = useNavigate();
-    let [currentAccomodation, setCurrentAccomodation] = useState(null);
+  const id = useParams();
+  const navigate = useNavigate();
+  let [currentAccomodation, setCurrentAccomodation] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
     fetch("/logements.json")
       .then((response) => response.json())
       .then((data) => {
@@ -28,20 +27,42 @@ function AccomodationPage() {
       });
   }, [navigate, id.id]);
 
-
   return (
     <Fragment>
-      <Header />
-      {currentAccomodation &&
-      (<Fragment>
-        <div className="accomodation--flex">
-             <div className="accomodation--tags__container">
+      <Header />{" "}
+      {currentAccomodation && (
+        <Fragment>
+          <div className="accomodation--flex">
+            <div>
+              <div>
+                <h2 className="accomodation--text accomodation--title">
+                  {currentAccomodation.title}
+                </h2>
+                <span className="accomodation--text accomodation--location">
+                  {currentAccomodation.location}
+                </span>
+              </div>
+              <div className="accomodation--tags__container">
                 {currentAccomodation.tags.map((tag, index) => (
                   <Tag key={index} index={index} tagTitle={tag} />
                 ))}
               </div>
-            <Rating starsNumber={currentAccomodation.rating} /></div>
-         <div className="accomodation--flex">
+            </div>
+            <div>
+              <div className="accomodation--host__container">
+                <div className="accomodation--text accomodation--host__name">
+                  {currentAccomodation.host.name}
+                </div>
+                <img
+                  src={currentAccomodation.host.picture}
+                  alt="Host"
+                  className="accomodation--host__picture"
+                />
+              </div>
+              <Rating starsNumber={currentAccomodation.rating} />
+            </div>
+          </div>
+          <div className="accomodation--flex">
             <Dropdown
               paragraph={currentAccomodation.description}
               title="Description"
@@ -51,8 +72,8 @@ function AccomodationPage() {
               title="Équipements"
             />
           </div>
-      </Fragment>)}
-        
+        </Fragment>
+      )}
       <Footer />
     </Fragment>
   );
